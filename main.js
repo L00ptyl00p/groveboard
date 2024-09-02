08a8f5836e27daaa1c5235b01d3dae3fbd41624c
L00ptyl00p <guadalupelucio27@gmail.com>
2024-09-01 22:44:26 -0700

    Update
    css
    js
    index

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-alert("Congratulations Erin");
+alert("Congratulations Erin !!!");
+
+function change() {
+  let currentWeek = document.querySelector("#current-week");
+  let h2innerHTML = currentWeek.h2innerHTML;
+  let h2 = currentWeek.querySelector("h2");
+  h2.innerHTML = "Congratulations Erin !!!";
+
+  h2.innerHTML = h2innerHTML;
+}
