00bedced22fc3c68794f9f677975be78e2580547
L00ptyl00p <guadalupelucio27@gmail.com>
2024-09-01 23:23:35 -0700

    update

--- main.js
+++ main.js
@@ -1,11 +1,1 @@
 alert("Congratulations Erin !!!");
-
-function change() {
-  let currentWeek = document.querySelector("#current-week");
-  let h2innerHTML = currentWeek.h2innerHTML;
-  let h2 = currentWeek.querySelector("h2");
-  h2.innerHTML =
-    "This week congratulations goes to Erin! 🎉🎉🎉, Erin's most memorable quote is 'Fuck this Job'";
-
-  currentWeekh2.innerHTML = h2innerHTML;
-}
