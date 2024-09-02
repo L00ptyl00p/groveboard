280fb6a8b1dbd0577f86c22bb4130a3890d9b757
L00ptyl00p <guadalupelucio27@gmail.com>
2024-09-01 23:41:54 -0700

    update

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 alert("Congratulations Erin !!!");
+function updateWeeks() {
+  let weeks = document.querySelector("#weeks");
+  let h2 = document.querySelector("h2");
+  let img = document.querySelector("img");
+
+  h2.innerHTML = "Week " + weeks.value;
+  img.src = "images/week" + weeks.value + "1.jpg";
+}
+
+addEventListener.onclick = updateWeeks;
